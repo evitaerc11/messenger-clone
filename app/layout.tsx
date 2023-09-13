c02a43743facf342ccb903bfb539bfe004c80d34
NAM! <abcdxyz14144@gmail.com>
2023-09-13 20:37:23 +0700

    Sidebar & Navigation & Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,25 +2,28 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import ToasterContext from './context/ToasterContext'
+import AuthContext from './context/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Messenger',
-  description: 'Messenger clone',
+  description: 'Messenger clone'
 }
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <ToasterContext />
-        {children}
-        </body>
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
+      </body>
     </html>
   )
 }
